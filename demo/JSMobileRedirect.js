87c1461e9f90da5d5e6cb1b3af50138f7b9c953d
root <root@m1.(none)>
2014-03-28 11:27:50 +0100

    Added demo directory

---
+++ demo/JSMobileRedirect.js
@@ -0,0 +1,21 @@
+function JSMobileRedirect()
+{
+    var userAgent = navigator.userAgent;
+    userAgent = userAgent.toLowerCase();
+    
+    if (userAgent.indexOf("iphone") >=0)
+    {
+	return "iphone";
+    }
+    else if (userAgent.indexOf("android") >=0 && userAgent.indexOf("mobile") >=0)
+    {
+	return "android";
+    }
+    else if (userAgent.indexOf("windows ce") >=0)
+    {
+	return "windows";
+    } else {
+	return "other";
+    }
+}
+
